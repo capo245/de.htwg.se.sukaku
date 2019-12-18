a0e73bff10b2d5371f09e2846d70a7576296ad27
capo245 <daniel.castelo@live.de>
2019-12-18 15:50:41 +0100

    moved conrtoller to package, changing controller with undo manager

--- src/main/scala/de/htwg/se/sukaku/controller/controllerComponent/controllerBaseImpl/Controller.scala
+++ src/main/scala/de/htwg/se/sukaku/controller/controllerComponent/controllerBaseImpl/Controller.scala
@@ -1,38 +1,87 @@
 package de.htwg.se.sukaku.controller.controllerComponent.controllerBaseImpl
 
-import de.htwg.se.sukaku.fascade.GridControllerFacade
-import de.htwg.se.sukaku.model.{Grid, Solver}
-import de.htwg.se.sukaku.util.Observable
+import de.htwg.se.sukaku.controller.controllerComponent.GameStatus._
+import de.htwg.se.sukaku.controller.controllerComponent._
+import de.htwg.se.sukaku.model.gridComponent.GridInterface
+import de.htwg.se.sukaku.model.gridComponent.gridBaseImpl.Grid
+import de.htwg.se.sukaku.util.UndoManager
 
-class Controller(var grid:Grid) extends Observable{
-  var facade = new GridControllerFacade(grid)
+import scala.swing.Publisher
 
-  def createEmptyGrid(size: Int):Unit = {
-//    grid = new Grid(size)
-    facade.gridfacade(size, 0, 0, 0, 0, "empty")
-    notifyObservers
+class Controller(var grid: GridInterface) extends ControllerInterface with Publisher {
+
+  var gameStatus: GameStatus = IDLE
+  var showAllCandidates: Boolean = false
+  private val undoManager = new UndoManager
+
+  def createEmptyGrid(size: Int): Unit = {
+    grid = new Grid(size)
+    publish(new CellChanged)
   }
 
-  def createRandomGrid(size: Int, randomCells:Int):Unit = {
-//    grid = new GridCreator(size).createRandom(randomCells)
-    facade.gridfacade(size, randomCells, 0, 0, 0, "random")
-    notifyObservers
+  def resize(newSize:Int) :Unit = {
+    grid = new Grid(newSize)
+    gameStatus=RESIZE
+    publish(new GridSizeChanged(newSize))
+  }
+
+  def createRandomGrid(size: Int, randomCells: Int): Unit = {
+    grid = grid.createNewGrid(size)
+    gameStatus = NEW
+    publish(new CellChanged)
   }
 
   def gridToString: String = grid.toString
 
-  def set(row: Int, col: Int, value: Int):Unit = {
-//    grid = grid.set(row, col, value)
-      facade.gridfacade(0, 0, row, col, value, "set")
-      notifyObservers
+  def set(row: Int, col: Int, value: Int): Unit = {
+    undoManager.doStep(new SetCommand(row, col, value, this))
+    gameStatus = SET
+    publish(new CellChanged)
   }
 
-  def solve: Boolean = {
+  def solve: Unit = {
+    undoManager.doStep(new SolveCommand(this))
+    gameStatus = SOLVED
+    publish(new CellChanged)
+  }
 
-    val (success, g) = new Solver(grid).solve
-    grid = g
-    notifyObservers
-    success
+  def undo: Unit = {
+    undoManager.undoStep
+    gameStatus = UNDO
+    publish(new CellChanged)
+  }
+
+  def redo: Unit = {
+    undoManager.redoStep
+    gameStatus = REDO
+    publish(new CellChanged)
+  }
+
+  def cell(row:Int, col:Int) = grid.cell(row,col)
+
+  def isGiven(row: Int, col: Int):Boolean = grid.cell(row, col).given
+  def isSet(row:Int, col:Int):Boolean = grid.cell(row, col).isSet
+  def available(row:Int, col:Int):Set[Int] = grid.available(row, col)
+  def showCandidates(row:Int, col:Int):Unit = {
+    grid=grid.setShowCandidates(row, col)
+    gameStatus = CANDIDATES
+    publish(new CandidatesChanged)
+  }
+
+  def isShowCandidates(row:Int, col:Int):Boolean = grid.cell(row, col).showCandidates
+  def gridSize:Int = grid.size
+  def blockSize:Int = Math.sqrt(grid.size).toInt
+  def isShowAllCandidates:Boolean = showAllCandidates
+  def toggleShowAllCandidates:Unit = {
+    showAllCandidates = !showAllCandidates
+    gameStatus = CANDIDATES
+    publish(new CellChanged)
+  }
+  def isHighlighted(row:Int, col: Int):Boolean = grid.isHighlighted(row, col)
+  def statusText:String = GameStatus.message(gameStatus)
+  def highlight(index:Int):Unit = {
+    grid = grid.highlight(index)
+    publish(new CellChanged)
   }
 
 }
