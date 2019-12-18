d8a8805e8c4e7b4eb902160f38c55f508ee2efc5
capo245 <daniel.castelo@live.de>
2019-12-18 15:51:53 +0100

    Added set structure for command pattern

---
+++ src/main/scala/de/htwg/se/sukaku/controller/controllerComponent/controllerBaseImpl/SetCommand.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.sukaku.controller.controllerComponent.controllerBaseImpl
+
+import de.htwg.se.sukaku.util.Command
+
+class SetCommand(row:Int, col: Int, value:Int, controller: Controller) extends Command {
+  override def doStep: Unit =   controller.grid = controller.grid.set(row, col, value)
+
+  override def undoStep: Unit = controller.grid = controller.grid.set(row, col, 0)
+
+  override def redoStep: Unit = controller.grid = controller.grid.set(row, col, value)
+}
