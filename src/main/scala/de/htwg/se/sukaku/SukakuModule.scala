4d0acc53d62b2aecc0b3030218964f0c4eb19f39
capo245 <daniel.castelo@live.de>
2020-01-09 15:28:10 +0100

    Added Guice to new MOdule. Preparing DI#2

---
+++ src/main/scala/de/htwg/se/sukaku/SukakuModule.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.sukaku
+
+class SukakuModule {
+  import com.google.inject.AbstractModule
+  import com.google.inject.name.Names
+  import net.codingwell.scalaguice.ScalaModule
+  import de.htwg.se.sukaku.controller.controllerComponent._
+  import de.htwg.se.sukaku.model.gridComponent.GridInterface
+  import de.htwg.se.sukaku.model.gridComponent.gridAdvancedImpl.Grid
+}
